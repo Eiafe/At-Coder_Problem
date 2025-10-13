1920a8a952634ec42cf7afbc300b3a1aad3f001b
Eiafe <mahathirullahyafikhan@gmail.com>
2025-10-14 01:05:34 +0600

    Add files via upload

---
+++ Contest_Problem/Circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    double circumference,Circle,r;
+    scanf("%lf",&r);
+     Circle=M_PI*r*r;
+     circumference=2*M_PI*r;
+     printf("%lf %lf",Circle,circumference);
+
+}
